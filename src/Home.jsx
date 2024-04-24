575ca3b46ea1de44287acfe55984eb6f410129bd
PerthTheIamCat <thongcum2546@gmail.com>
2024-04-24 20:26:14 +0700

    New : Playing with Framer animation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,23 @@
 import Navbar from "./Navbar"
 import Sec1 from "./FirstText"
+import { useScroll, useMotionValueEvent} from "framer-motion"
+import { useRef } from "react"
 
 const Home = () => {
-    
+    const targetRef = useRef(null)
+    const { scrollY } = useScroll({
+      container: targetRef,
+    });
+    useMotionValueEvent(scrollY, "change", (lasted) => {
+      console.log(lasted);
+    });
+
     return (
     <>
         <div className="bg-gradient-to-tr from-cyan-500 from-35% via-emerald-400 via-50% to-violet-400">
             <Navbar/>
-            <div className="max-h-[100vh] snap-y snap-mandatory overflow-scroll scroll-smooth">
-                <div className="h-[100vh] snap-cente ">
+            <div ref={targetRef} className="max-h-[100vh] snap-y snap-mandatory overflow-scroll scroll-smooth">
+                <div className="h-[100vh] snap-center ">
                     <Sec1/>
                 </div>
                 <div className="h-[100vh] snap-center">TEST</div>
