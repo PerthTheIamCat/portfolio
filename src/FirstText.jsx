433c2d4dbcc9a70122dfbe2bcc7065effe219275
PerthTheIamCat <thongcum2546@gmail.com>
2024-04-25 15:50:50 +0700

    adj : play with more animation

--- src/FirstText.jsx
+++ src/FirstText.jsx
@@ -1,11 +1,18 @@
 import { motion } from "framer-motion"
+import TypewriterComponent from "typewriter-effect"
 
 const Section1 = ({ scale, rotate, opacity }) =>{
     return(
         <>
             <div className="flex justify-center h-[100vh]">
                 <motion.div style={{scale, rotate, opacity}} className="flex">
-                    <h1 className="text-8xl place-self-center ">Hello World</h1>
+                    <h1 className="text-8xl text-slate-300 place-self-center">
+                    <TypewriterComponent options={{
+                        strings : ["Hello", "World"],
+                        loop : true,
+                        autoStart : true,
+                    }}/>
+                    </h1>
                 </motion.div>
             </div>
         </>
--- src/Home.jsx
+++ src/Home.jsx
@@ -15,17 +15,22 @@ const Home = () => {
     const scale = useTransform(scrollY, [0, 400], [1, 10]);
     const rotate = useTransform(scrollY, [0, 400], ["0deg", ["180deg"]]);
     const opacity = useTransform(scrollY, [0, 400], [1, 0]);
-
+    const scale2 = useTransform(scrollY, [400, 800], [10, 1]);
+    const scale3 = useTransform(scrollY, [800, 1600], [10, 1]);
     return (
     <>
         <div className="bg-gradient-to-tr from-cyan-500 from-35% via-emerald-400 via-50% to-violet-400">
             <Navbar/>
-            <div ref={targetRef} className="max-h-[100vh] snap-y snap-mandatory overflow-scroll scroll-smooth opa" >
+            <div ref={targetRef} className="max-h-[100vh] snap-y snap-mandatory overflow-scroll scroll-smooth bg-sky-950">
                 <motion.div className="h-[100vh] snap-center ">
-                    <Sec1 scale={scale} rotate={rotate} opacity={opacity}/>
+                  <Sec1 scale={scale} rotate={rotate} opacity={opacity}/>
                 </motion.div>
-                <div className="h-[100vh] snap-center">TEST</div>
-                <div className="h-[100vh] snap-center">TEST</div>
+                <motion.div className="h-[100vh] snap-center" >
+                  <Sec1 scale={scale2}/>
+                </motion.div>
+                <motion.div className="h-[100vh] snap-center">
+                  <Sec1 scale={scale3}/>
+                </motion.div>
             </div>
         </div>
 
